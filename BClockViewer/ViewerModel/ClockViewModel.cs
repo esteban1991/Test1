82b84f8aa21e9791990bda10c4eeb47a7c63c681
Esteban <Esteban@ESTEBAN>
2018-06-02 15:33:43 +0200

    Agregar archivos de proyecto.

---
+++ BClockViewer/ViewerModel/ClockViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Linq;
+using System.Text;
+using BerlinClock;
+using System.Windows.Threading;
+using BClockViewer.Viewer;
+namespace BClockViewer.Viewer
+{
+    class ClockViewModel : INotifyPropertyChanged
+    {
+        private readonly DispatcherTimer _timer;
+        private readonly CberlinClock _clock;
+        private long _counter = 0;
+
+        public string TheTime
+        {
+            get
+            {
+                return _clock.Generate(DateTime.Now);
+            }
+        }
+
+        public long Counter
+        {
+            get
+            {
+                return _counter;
+            }
+            set
+            {
+                _counter = value;
+                OnPropertyChanged("Counter");
+            }
+        }
+
+        public ClockViewModel()
+        {
+            _clock = new CberlinClock();
+
+            _timer = new DispatcherTimer();
+            _timer.Interval = new TimeSpan(0, 0, 0, 0, 5);
+            _timer.Tick += TimerTick;
+            _timer.Start();
+        }
+
+        void TimerTick(object sender, EventArgs e)
+        {
+            Counter++;
+            OnPropertyChanged("TheTime");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
---
+++ BDD/BerlinClockFeatureSteps.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Globalization;
+
+namespace BerlinClock
+{
+    [Binding]
+    public class TheBerlinClockSteps
+    {
+        //private ITimeConverter berlinClock = new ITimeConverter();
+        private string theTime;
+
+
+        public ITimeConverter BerlinClock {  get => this.BerlinClock; set => this.BerlinClock = value;  }
+
+        [When(@"the time is ""(.*)""")]
+        public void WhenTheTimeIs(string time)
+        {
+            theTime = time;
+        }
+        
+        [Then(@"the clock should look like")]
+        public void ThenTheClockShouldLookLike(string theExpectedBerlinClockOutput)
+        {
+
+            try
+            {
+
+                var arclock = new CberlinClock();
+                Assert.AreEqual(arclock.Generate(Convert.ToDateTime(theTime)), theExpectedBerlinClockOutput);
+              
+            }
+            catch (Exception ex) { throw ex; }
+        }
+
+    }
+}
